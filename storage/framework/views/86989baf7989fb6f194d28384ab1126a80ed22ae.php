0d773da61578bac4c6ae2bfb4dbd26ef61a0d132
Vincent Roy <vrhack1@gmail.com>
2024-05-21 12:39:29 +0200

    Add files via upload

---
+++ storage/framework/views/86989baf7989fb6f194d28384ab1126a80ed22ae.php
@@ -0,0 +1,69 @@
+<?php if (isset($component)) { $__componentOriginalc254754b9d5db91d5165876f9d051922ca0066f4 = $component; } ?>
+<?php $component = Illuminate\View\AnonymousComponent::resolve(['view' => 'components.layout','data' => []] + (isset($attributes) && $attributes instanceof Illuminate\View\ComponentAttributeBag ? (array) $attributes->getIterator() : [])); ?>
+<?php $component->withName('layout'); ?>
+<?php if ($component->shouldRender()): ?>
+<?php $__env->startComponent($component->resolveView(), $component->data()); ?>
+<?php if (isset($attributes) && $attributes instanceof Illuminate\View\ComponentAttributeBag && $constructor = (new ReflectionClass(Illuminate\View\AnonymousComponent::class))->getConstructor()): ?>
+<?php $attributes = $attributes->except(collect($constructor->getParameters())->map->getName()->all()); ?>
+<?php endif; ?>
+<?php $component->withAttributes([]); ?>
+  <?php if (isset($component)) { $__componentOriginalc254754b9d5db91d5165876f9d051922ca0066f4 = $component; } ?>
+<?php $component = Illuminate\View\AnonymousComponent::resolve(['view' => 'components.card','data' => ['class' => 'p-10']] + (isset($attributes) && $attributes instanceof Illuminate\View\ComponentAttributeBag ? (array) $attributes->getIterator() : [])); ?>
+<?php $component->withName('card'); ?>
+<?php if ($component->shouldRender()): ?>
+<?php $__env->startComponent($component->resolveView(), $component->data()); ?>
+<?php if (isset($attributes) && $attributes instanceof Illuminate\View\ComponentAttributeBag && $constructor = (new ReflectionClass(Illuminate\View\AnonymousComponent::class))->getConstructor()): ?>
+<?php $attributes = $attributes->except(collect($constructor->getParameters())->map->getName()->all()); ?>
+<?php endif; ?>
+<?php $component->withAttributes(['class' => 'p-10']); ?>
+    <header>
+      <h1 class="text-3xl text-center font-bold my-6 uppercase">
+        Manage Gigs
+      </h1>
+    </header>
+
+    <table class="w-full table-auto rounded-sm">
+      <tbody>
+        <?php if (! ($listings->isEmpty())): ?>
+        <?php $__currentLoopData = $listings; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $listing): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+        <tr class="border-gray-300">
+          <td class="px-4 py-8 border-t border-b border-gray-300 text-lg">
+            <a href="/listings/<?php echo e($listing->id); ?>"> <?php echo e($listing->title); ?> </a>
+          </td>
+          <td class="px-4 py-8 border-t border-b border-gray-300 text-lg">
+            <a href="/listings/<?php echo e($listing->id); ?>/edit" class="text-blue-400 px-6 py-2 rounded-xl"><i
+                class="fa-solid fa-pen-to-square"></i>
+              Edit</a>
+          </td>
+          <td class="px-4 py-8 border-t border-b border-gray-300 text-lg">
+            <form method="POST" action="/listings/<?php echo e($listing->id); ?>">
+              <?php echo csrf_field(); ?>
+              <?php echo method_field('DELETE'); ?>
+              <button class="text-red-500"><i class="fa-solid fa-trash"></i> Delete</button>
+            </form>
+          </td>
+        </tr>
+        <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
+        <?php else: ?>
+        <tr class="border-gray-300">
+          <td class="px-4 py-8 border-t border-b border-gray-300 text-lg">
+            <p class="text-center">No Listings Found</p>
+          </td>
+        </tr>
+        <?php endif; ?>
+
+      </tbody>
+    </table>
+   <?php echo $__env->renderComponent(); ?>
+<?php endif; ?>
+<?php if (isset($__componentOriginalc254754b9d5db91d5165876f9d051922ca0066f4)): ?>
+<?php $component = $__componentOriginalc254754b9d5db91d5165876f9d051922ca0066f4; ?>
+<?php unset($__componentOriginalc254754b9d5db91d5165876f9d051922ca0066f4); ?>
+<?php endif; ?>
+ <?php echo $__env->renderComponent(); ?>
+<?php endif; ?>
+<?php if (isset($__componentOriginalc254754b9d5db91d5165876f9d051922ca0066f4)): ?>
+<?php $component = $__componentOriginalc254754b9d5db91d5165876f9d051922ca0066f4; ?>
+<?php unset($__componentOriginalc254754b9d5db91d5165876f9d051922ca0066f4); ?>
+<?php endif; ?>
+<?php /**PATH C:\xampp\htdocs\lrvl-WiederK\resources\views/listings/manage.blade.php ENDPATH**/ ?>
---
+++ storage/framework/views/bdd45c2a9a316b6d119610e95853df905dc83c4c.php
@@ -0,0 +1,4 @@
+<div <?php echo e($attributes->merge(['class' => 'bg-gray-50 border border-gray-200 rounded p-6'])); ?>>
+  <?php echo e($slot); ?>
+
+</div><?php /**PATH C:\xampp\htdocs\lrvl-WiederK\resources\views/components/card.blade.php ENDPATH**/ ?>
